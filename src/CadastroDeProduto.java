4d2cf0935a6f4ee1ce0f96438bdf212992d46285
GolfBravoSierra <106974254+GolfBravoSierra@users.noreply.github.com>
2023-10-03 15:02:34 -0300

    03/10/23 - inicio bem sucedido

--- src/CadastroDeProduto.java
+++ src/CadastroDeProduto.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class CadastroDeProduto {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Produto[] produto = new Produto[100]    ; 
+
+        System.out.println("Escolha um opção do meno abaixo:");
+        System.out.println("1 - Cadastrar produto");
+        System.out.println("2 - Cadastra produto perecivel");
+        System.out.println("3 - Sair do Programa");
+
+        try (Scanner input = new Scanner(System.in)) {
+            // código que usa o objeto Scanner
+            int reposta = input.nextInt();
+    
+            switch (reposta) {
+                case 1:
+                    System.out.println("---Cadastrar produto---\n");
+                    System.out.println("Digite a descrição do produto:");
+                    String descricao = input.next();
+                    System.out.println("Digite o valor do produto:");
+                    double valor = input.nextDouble();
+                    int aux = Produto.getQuantidade();
+                    produto[aux] = new Produto(descricao, valor);
+                    break;
+                case 2:
+                    System.out.println("Cadastra produto perecivel");
+                    break;
+                case 3:
+                    System.out.println("Sair do Programa");
+                    break;
+                default:
+                    System.out.println("Opção invalida");
+                    break;
+            }
+        }
+
     }
 }
+
+
---
+++ src/Produto.java
@@ -0,0 +1,53 @@
+public class Produto {
+    private String descricao;   //Descricao do produto
+    private int id;             //ID do produto
+    private double valor;       //Valor do produto
+    private static int quantidade = 0 ;     //Quantidade de produtos
+    
+    public Produto(String descricao, double valor){
+        this.descricao = descricao;
+        this.valor = valor;
+        quantidade++;
+        this.id = quantidade;
+    }
+
+    //Metodo retorna descricao
+    public String getDescricao(){
+        return this.descricao;
+    }
+
+    //Metodo retorna ID
+    public int getId(){
+        return this.id;
+    }
+
+    //Metodo retorna valor
+    public double getValor(){
+        return this.valor;
+    }
+
+    //Metodo retorna quantidade
+    public static int getQuantidade(){
+        return Produto.quantidade;
+    }
+
+    //Metodo recebe descricao
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    //Metodo recebe ID
+    public void setId(int id){
+        this.id = id;
+    }
+
+    //Metodo recebe valor
+    public void setValor(double valor){
+        this.valor = valor;
+    }
+
+    //Metodo retorna quantidade
+    public String toString(){
+        return "ID: " + this.id + " Descricao: " + this.descricao + " Valor: " + this.valor;
+    }
+} 
